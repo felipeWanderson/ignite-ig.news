e8a3f2f9ef9a9d980708f83d4e746a574920cb0c
Felipe Leal <felipewanderson2020@gmail.com>
2021-04-04 21:45:48 -0300

    (UI) botão SignIn

---
+++ src/components/SignInButton/index.tsx
@@ -0,0 +1,25 @@
+import {FaGithub} from 'react-icons/fa';
+import {FiX} from 'react-icons/fi';
+
+import styles from './styles.module.scss';
+export function SignInButton() {
+  const isLoggedIn = false;
+  return isLoggedIn ? (
+    <button 
+      type="button"
+      className={styles.signInButton}
+    >
+      <FaGithub color="#04d361"/>
+      Felipe Leal
+      <FiX color="#737380" className={styles.closeIcon}/>
+    </button> 
+  ): (
+    <button 
+      type="button"
+      className={styles.signInButton}
+    >
+      <FaGithub color="#eba417"/>
+      Sign in with GitHub
+    </button> 
+  );
+}
