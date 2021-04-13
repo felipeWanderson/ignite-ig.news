b8e8af2b039934df0def0e293168df66b19ae5f2
Felipe Leal <felipewanderson2020@gmail.com>
2021-04-13 00:22:27 -0300

    connectando com api do stripe para o checkout

--- src/components/SubscribeButton/index.tsx
+++ src/components/SubscribeButton/index.tsx
@@ -1,4 +1,5 @@
 import { signIn, useSession } from 'next-auth/client';
+import { getStripeJs } from '../../services/stripe-js';
 import styles from './styles.module.scss';
 
 interface SubscribeButtonProps {
@@ -6,7 +7,7 @@ interface SubscribeButtonProps {
 }
 export function SubscribeButton({priceId}: SubscribeButtonProps) {
   const [session] = useSession();
-  function handleSubscribe() {
+  async function handleSubscribe() {
     if (!session) {
       signIn('github');
       return;
@@ -14,10 +15,26 @@ export function SubscribeButton({priceId}: SubscribeButtonProps) {
 
     // cria checkout session
     
-    
+    try { 
+      const response = await fetch('/api/subscribe', {
+        method: 'POST',
+      });
+      const responseJSON = await response.json();
+      const { sessionId } = responseJSON;
+
+      const stripe = await getStripeJs();
+
+      await stripe.redirectToCheckout({sessionId});
+    } catch (err) {
+      alert(err.message);
+    }
   }
   return (
-    <button type="button" className={styles.subscribeButton}>
+    <button 
+      type="button" 
+      className={styles.subscribeButton}
+      onClick={handleSubscribe}
+      >
       Subscribe Now
     </button>
   );
