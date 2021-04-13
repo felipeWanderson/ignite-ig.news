813f7f80ddadd8c5c0bbb0f77538732194942b5e
Felipe Leal <felipewanderson2020@gmail.com>
2021-04-13 00:56:04 -0300

    verificando se exitem usuarios duplicados mo stripe

--- src/pages/api/subscribe.ts
+++ src/pages/api/subscribe.ts
@@ -1,17 +1,56 @@
 import { NextApiRequest, NextApiResponse } from "next";
 import { getSession } from "next-auth/client";
+import { fauna } from "../../services/fauna";
+import { query as q } from 'faunadb';
 import { stripe } from "../../services/stripe";
 
+type User = {
+  ref: {
+    id: string;
+  }
+  data: { 
+    stripe_costomer_id: string;
+  }
+}
 export default async (request: NextApiRequest, response: NextApiResponse) => {
   if(request.method === 'POST') {
     const session = await getSession({ req: request });
 
-    const stripeCostumer = await stripe.customers.create({
-      email: session.user.email,
-      // metadata
-    })
+    const user = await fauna.query<User>(
+      q.Get(
+        q.Match(
+          q.Index('user_by_email'),
+          q.Casefold(session.user.email),
+        )
+      )
+    );
+
+    let costomerId = user.data.stripe_costomer_id;
+    
+    if (!costomerId) {
+      const stripeCostumer = await stripe.customers.create({
+        email: session.user.email,
+        // metadata
+      });
+
+      await fauna.query(
+        q.Update(
+          q.Ref(q.Collection('users'), user.ref.id),
+          {
+            data: {
+              stripe_costomer_id: stripeCostumer.id,
+            }
+          }
+        )
+      );
+
+      costomerId = stripeCostumer.id;
+    }
+    
+
+    
     const stripeCheckoutSession = await stripe.checkout.sessions.create({
-      customer: stripeCostumer.id,
+      customer: costomerId,
       payment_method_types: ['card'],
       billing_address_collection: 'required',
       line_items: [
