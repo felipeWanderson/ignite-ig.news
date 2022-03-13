859229f84f84dbba341a040a89dc3619789e0ce7
Felipe Leal <felipe.leal@compasso.com.br>
2022-03-13 13:30:55 -0300

    conecção com API prismic e listando posts

--- src/pages/posts/index.tsx
+++ src/pages/posts/index.tsx
@@ -1,53 +1,72 @@
 
+import { GetStaticProps } from 'next';
 import Head from 'next/head';
+import Prismic from '@prismicio/client';
+import { RichText } from 'prismic-dom';
+import {format} from 'date-fns';
+import {ptBR} from 'date-fns/locale';
+import { getPrismicClient } from '../../services/prismic';
 import styles from './styles.module.scss';
 
-export default function Posts() {
+interface Post {
+  slug: string;
+  title: string;
+  excerpt: string;
+  updatedAt: string;
+}
+interface PostsProps {
+  posts: Post[]
+}
+export default function Posts({ posts }: PostsProps) {
   return (
-  <>
-    <Head>
-      <title>Posts | Ignews</title>
-    </Head>
+    <>
+      <Head>
+        <title>Posts | Ignews</title>
+      </Head>
 
-    <main className={styles.container}>
-      <div className={styles.posts}>
-        <a href="#">
-          <time>11 de Março de 2022</time>
-          <strong>Isso é um titulo</strong>
-          <p>Isso é um paragrafo.Isso é um paragrafo.Isso é um paragrafo.Isso é um paragrafo.Isso é um paragrafo.Isso é um paragrafo.Isso é um paragrafo.Isso é um paragrafo.Isso é um paragrafo.Isso é um paragrafo.</p>  
-        </a>
-        <a href="#">
-          <time>11 de Março de 2022</time>
-          <strong>Isso é um titulo</strong>
-          <p>Isso é um paragrafo.Isso é um paragrafo.Isso é um paragrafo.Isso é um paragrafo.Isso é um paragrafo.Isso é um paragrafo.Isso é um paragrafo.Isso é um paragrafo.Isso é um paragrafo.Isso é um paragrafo.</p>  
-        </a>
-        <a href="#">
-          <time>11 de Março de 2022</time>
-          <strong>Isso é um titulo</strong>
-          <p>Isso é um paragrafo.Isso é um paragrafo.Isso é um paragrafo.Isso é um paragrafo.Isso é um paragrafo.Isso é um paragrafo.Isso é um paragrafo.Isso é um paragrafo.Isso é um paragrafo.Isso é um paragrafo.</p>  
-        </a>
-        <a href="#">
-          <time>11 de Março de 2022</time>
-          <strong>Isso é um titulo</strong>
-          <p>Isso é um paragrafo.Isso é um paragrafo.Isso é um paragrafo.Isso é um paragrafo.Isso é um paragrafo.Isso é um paragrafo.Isso é um paragrafo.Isso é um paragrafo.Isso é um paragrafo.Isso é um paragrafo.</p>  
-        </a>
-        <a href="#">
-          <time>11 de Março de 2022</time>
-          <strong>Isso é um titulo</strong>
-          <p>Isso é um paragrafo.Isso é um paragrafo.Isso é um paragrafo.Isso é um paragrafo.Isso é um paragrafo.Isso é um paragrafo.Isso é um paragrafo.Isso é um paragrafo.Isso é um paragrafo.Isso é um paragrafo.</p>  
-        </a>
-        <a href="#">
-          <time>11 de Março de 2022</time>
-          <strong>Isso é um titulo</strong>
-          <p>Isso é um paragrafo.Isso é um paragrafo.Isso é um paragrafo.Isso é um paragrafo.Isso é um paragrafo.Isso é um paragrafo.Isso é um paragrafo.Isso é um paragrafo.Isso é um paragrafo.Isso é um paragrafo.</p>  
-        </a>
-        <a href="#">
-          <time>11 de Março de 2022</time>
-          <strong>Isso é um titulo</strong>
-          <p>Isso é um paragrafo.Isso é um paragrafo.Isso é um paragrafo.Isso é um paragrafo.Isso é um paragrafo.Isso é um paragrafo.Isso é um paragrafo.Isso é um paragrafo.Isso é um paragrafo.Isso é um paragrafo.</p>  
-        </a>
-      </div>
-    </main>
-  </>
+      <main className={styles.container}>
+        <div className={styles.posts}>
+          {posts.map(post => (
+            <a key={post.slug} href="#">
+              <time>{post.updatedAt}</time>
+              <strong>{post.title}</strong>
+              <p>{post.excerpt}</p>
+            </a>
+          ))}
+        </div>
+      </main>
+    </>
   );
+}
+
+export const getStaticProps: GetStaticProps = async () => {
+
+  const prismic = getPrismicClient();
+
+  const response = await prismic.query(
+    [
+      Prismic.predicates.at('document.type', 'publication'),
+    ],
+    {
+      fetch: ['publication.title', 'publication.content'],
+      pageSize: 100,
+    }
+  );
+
+  const posts = response.results.map(post => {
+    return {
+      slug: post.uid,
+      title: RichText.asText(post.data.title),
+      excerpt: post.data.content.find(content => content.type === 'paragraph')?.text ?? '',
+      updatedAt: format(new Date(post.last_publication_date), `dd 'de' MMMMMMM 'de' yyyy`, {
+        locale: ptBR
+      }),
+    }
+  });
+
+  return {
+    props: {
+      posts,
+    }
+  }
 }
